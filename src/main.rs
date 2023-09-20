e8b0f388ee9024576d53189f80439ebb0aeb6745
esfalsa <pronoun.nationstates@gmail.com>
2023-09-19 21:52:50 -0700

    accept user nation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use clap::Parser;
+
+/// A command-line utility for generating NationStates region update timesheets
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Cli {
+    /// The name of your nation, to identify you to NationStates
+    #[arg(short = 'n', long = "nation")]
+    user_nation: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    println!("Running srsglass with user nation {}", args.user_nation);
 }
